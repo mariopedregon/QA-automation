8d8bf4f68100f06d5da14bda7239960a0bf0771f
Mario Pedregon <mariopedregon93@gmail.com>
2019-06-06 17:58:42 -0600

    first commit

---
+++ stg.test/UnitTest1.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Tests
+{
+    public class Tests
+    {
+        string ROOT_PATH = Path.GetFullPath(@"../../../../");
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            driver = new ChromeDriver(ROOT_PATH + "/_drivers");
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void AfterEach() { driver.Quit(); }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://google.com";
+            Assert.That(driver.Title.Contains("Google"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            driver.Url = "https://copart.com";
+            driver.FindElement(By.Id("input-search")).SendKeys("exotics" + Keys.Enter);
+
+            wait.Until(drvr => drvr.FindElement(By.Id("serverSideDataTable")).Displayed);
+
+            Assert.That(driver.PageSource.Contains("PORSCHE"));
+            driver.FindElement(By.CssSelector("[ng-click=`search()`]")).Click();
+
+        }
+    }
+}
